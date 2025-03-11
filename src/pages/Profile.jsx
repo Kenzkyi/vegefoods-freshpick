cfc273b45d50be55c0707505c5e467fd3cc130dc
Osuji Wisdom <wisdomosuji26@gmail.com>
2025-03-11 18:01:43 +0100

    added some changes to the profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../styles/profile.css"
+import { RiLogoutBoxFill } from "react-icons/ri";
+import {useNavigate} from 'react-router-dom'
+import { TbCloudUpload } from "react-icons/tb";
+import { BsFillCameraFill } from "react-icons/bs";
+import EditPopUp from '../components/EditPopUp';
 
 const Profile = () => {
+
+  const navigate = useNavigate()
+
+  const [imageValue, setImageValue] = useState(null)
+  const [isPopUpOpend, setIsPopupOpen] = useState(false)
+
+    const getImageUrl = (e) => {
+      const file = e.target.files[0];
+      if (file && file.type.startsWith("image/")) {
+        const url = URL.createObjectURL(file)
+        setImageValue(url)
+      } else {
+          alert("please upload a valid image file!");
+      }
+     
+    }
+
   return (
     <div className='Profile'>
+
+         <EditPopUp isopen={isPopUpOpend} onClose={()=>setIsPopupOpen(false)} >
+
+<div className='popup-div-top'>
+    <div className='edit-image-div'>
+        <img src={imageValue} alt="" />
+                    <input onChange={getImageUrl} type="file" id='l' hidden className='my-file-input'/>
+            <label htmlFor="l" className='mylabel2' ><BsFillCameraFill style={{fontSize: "40px", fontWeight: "600", cursor: "pointer", color: "green"}} /></label>
+    </div>
+</div>
+<div className='popup-div-btm'>
+    <div className='user-details-edit-wrap'>
+        <div className='username-div-edit'>
+            <input type="text" className='my-edit-input' placeholder='Username' />
+        </div>
+        <div className='location-div-edit'>
+            <input type="text" className='my-edit-input'placeholder='Location' />
+        </div>
+        <div className='phone-number-div-edit'>
+            <input type="number" className='my-edit-input' placeholder='Phone Number' />
+        </div>
+    </div>
+    <div className='save-cahnage-cancel-btn'>
+        <div className='save-cahnage-cancel-btn-wrap'>
+            <button className='my-edit-popup-active-btn-cancel'>Cancel</button>
+            <button className='my-edit-popup-active-btn-save-changes'>Save Changes</button>
+        </div>
+    </div>
+</div>
+
+</EditPopUp>
+
         <div className='Profile-wrap'>
             <div className='Profile-wrap-up-div'>
                 <div className='image-div'>
+                <button onClick={()=>navigate("/")} className='logout-btn'><RiLogoutBoxFill/>Logout</button>
                     <div className='image-div-main'>
-                        <img src="/src/assets/public/453596388_122102941406449484_3412917747762962178_n.jpg" alt="img" />
+                    {
+                            imageValue ? (
+                                <img src={imageValue} alt="" />
+                            ) : (
+                                <>
+                                <input onChange={getImageUrl} type="file" id='l' hidden className='my-file-input'/>
+                        <label htmlFor="l" className='mylabel' ><TbCloudUpload style={{fontSize: "75px", fontWeight: "600", cursor: "pointer"}} /></label>
+                                </>
+                            )
+                        }
                     </div>
                 </div>
                 <div className='any-choice-div'>
+                <div className='bird-div'>
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIYUeh_jPiNdJAV8w-eAlcgjcYxstpo8AHOQ&s" alt="" />
+                    </div>
                     <div className='Account-and-payment'>
                         <button className='payment-btn'>Account & Payments</button>
                     </div>
@@ -19,10 +86,10 @@ const Profile = () => {
             </div>
             <div className='Profile-wrap-down-div'>
                 <div className='Name-div-wrap'>
-                    <div className='name-div-top'>OSUJI WISDOM</div>
+                    <div className='name-div-top'>wisdomosuji26</div>
                     <div className='name-div-btm'>
                         <div className='edit-profile'>
-                            <button className='myprofilebtn'>Edit Profile</button>
+                            <button onClick={()=>setIsPopupOpen(true)} className='myprofilebtn'>Edit Profile</button>
                         </div>
                         <div className='settings'>
                             <button className='myprofilebtn2'>Settings</button>
@@ -31,14 +98,12 @@ const Profile = () => {
                 </div>
                 <div className='User-details-div'>
                     <div className='requirement-div1'>
-                        <p>Username:</p>
                         <p>Location:</p>
                         <p>Phone:</p>
                         <p>Email:</p>
                         
                     </div>
                     <div className='requirement-div2'>
-                        <p>wisdomosuji26</p>
                         <p>Texas, United State.</p>
                         <p>+1 123 456 096</p>
                         <p>wisdomosuji26@gmail.com</p>
