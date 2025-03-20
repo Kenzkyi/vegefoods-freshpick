c0c69f28775b7f564449d3662288b03d578300f6
Kenz <ekeneokoye93@gmail.com>
2025-03-20 01:14:02 +0100

    sign up

--- src/auth/Signup.jsx
+++ src/auth/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "../styles/signup.css"
 import { NavLink, useNavigate } from "react-router-dom";
 import axios from 'axios';
@@ -10,157 +10,188 @@ const Signup = () => {
     email:'',
     fullName:'',
     password:'',
-    // usernameErr:false
+    usernameErr:'',
+    emailErr:'',
+    fullnameErr:'',
+    passwordErr:''
   })
+    const [isDisabled,setIsdisabled] = useState(true)
+    const [loading,setLoading] = useState(false)
   const nav = useNavigate()
 
-  const onChangeFunc = (e)=>{
-    const {name, value} = e.target
+  const validateEmail = (input) => {
+    // Regular expression for basic email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(input);
+  };
+
+  const onChangeFuncUsername = (e)=>{
+    const {value} = e.target
     const newUsername = value
-    setAll({...all,[name]:newUsername.trim()})
-    // if(newUsername){
-    //   console.log('there is username')
-    //   setAll({...all,usernameErr:false})
-    // }else{
-    //   setAll({...all,usernameErr:true})
-    // }
+    if(newUsername){
+      setAll({...all,usernameErr:'',username:newUsername.trim()})
+    }else{
+      setAll({...all,username:newUsername.trim(),usernameErr:'Please enter username'})
+    }
+  }
+  const onChangeFuncEmail = (e)=>{
+    const {value} = e.target
+    const newEmail = value
+    if(newEmail){
+      if (validateEmail(newEmail)) {
+        setAll({...all,email:newEmail.trim(),emailErr:''})
+      } else {
+        setAll({...all,email:newEmail.trim(),emailErr:'Please enter a valid email address'})
+      }
+    }else{
+      setAll({...all,email:newEmail.trim(),emailErr:'Please enter email address'})
+    }
+  }
+  const onChangeFuncFullname = (e)=>{
+    const {value} = e.target
+    const newFullname = value
+    if(newFullname){
+      setAll({...all,fullName:newFullname.trim(),fullnameErr:''})
+    }else{
+      setAll({...all,fullName:newFullname.trim(),fullnameErr:'Please enter your fullname'})
+    }
+  }
+  const onChangeFuncPassword = (e)=>{
+    const {value} = e.target
+    const newPassword = value
+    if(newPassword){
+      if (newPassword.length >= 6) {
+        setAll({...all,password:newPassword,passwordErr:''})
+      } else {
+        setAll({...all,password:newPassword,passwordErr:'Password must be at least 6 characters'})
+      }
+    }else{
+      setAll({...all,password:newPassword,passwordErr:'Please enter password'})
+    }
   }
 
 
     const baseUrl = 'https://vege-food.onrender.com/api/v1/'
 
-  const createUser = async (data) => {
+  const createUser = async (e,data) => {
+    e.preventDefault()
+    setIsdisabled(true)
+    setLoading(true)
     try{
-      const res = await axios.post(`${baseUrl}register`,data)
+      const res = await axios.post(`${baseUrl}register`,{username:data.username,email:data.email,fullName:data.fullName,password:data.password})
       toast.success('Sign up successful. please check your Email to verify')
+      setLoading(false)
       setTimeout(() => {
         nav('/login')
       }, 6000);
+      setIsdisabled(false)
     }catch(error){
       console.log(error)
       if(error.message === 'Network Error'){
         toast.error('Oops network error')
       }
+      setLoading(false)
+      setIsdisabled(false)
     }
   }
 
-  const submitData = ()=>{
-    if(all.username && all.email && all.fullName && all.password){
-      createUser(all)
+  // const submitData = ()=>{
+  //   if(all.username && all.emailErr === '' && all.fullName && all.passwordErr === ''){
+  //     createUser(all)
+  //   }else{
+  //     toast.error('Please all input field required')
+  //   }
+  // }
+
+  useEffect(()=>{
+    if(all.username && all.emailErr === '' && all.fullName && all.passwordErr === '' && all.password && all.email){
+      setIsdisabled(false)
     }else{
-      toast.error('Please all input field required')
+      setIsdisabled(true)
     }
-  }
-
-
+  },[all])
 
 
 
   return (
-    <div className='Body'>
-      <div className='LeftPart'>
-      <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-        background: "transparent",
-      }}
-    >
-
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          height:"70%",
-          gap: "20px",
-          padding: "20px",
-          background: "#ffffff7a",
-          alignItems:"center",
-          justifyContent:"center",
-          borderRadius: "8px",
-          boxShadow: "0px 0px 10px rgba(0,0,0,0.1)",
-          width: "70%",
-        }}
-      >
-              <h2 style={{fontSize:"30px"}}>Signup</h2>
-        <input
-          type="text"
-          name="username"
-          placeholder=" Username"
-          style={{
-            padding: "10px",
-            border: "1px solid #ccc",
-            borderRadius: "5px",
-            width:"80%",
-            height:"10%",
-            outline:'none',
-            borderColor:all.usernameErr?'red':'white'
-          }}
-          onChange={onChangeFunc}
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          style={{
-            padding: "10px",
-            border: "1px solid #ccc",
-            borderRadius: "5px",
-            width:"80%",
-            height:"10%"
-          }}
-          onChange={onChangeFunc}
-        />
-          <input
-          type="name"
-          name="fullName"
-          placeholder="Fullname"
-          style={{
-            padding: "10px",
-            border: "1px solid #ccc",
-            borderRadius: "5px",
-            width:"80%",
-            height:"10%"
-          }}
-          onChange={onChangeFunc}
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          style={{
-            padding: "10px",
-            border: "1px solid #ccc",
-            borderRadius: "5px",
-            width:"80%",
-            height:"10%"
-          }}
-          onChange={onChangeFunc}
-        />
-        <button
-          style={{
-            padding: "10px",
-            background: "blue",
-            color: "#fff",
-            border: "none",
-            borderRadius: "5px",
-            width:"80%",
-            height:"45px",
-            cursor: "pointer",
-          }}
-          onClick={submitData}
-        >
-          Signup
-        </button>
-        <p>Already Have an acoount ? <NavLink style={{cursor: "pointer", textDecoration: "none"}} to="/login"> log in</NavLink></p>
-      </div>
-    </div>
-      </div>
-      
-    </div>
+    <div className='Body2'>
+          <div className='LoginDiv'>
+        <main>
+          <form onSubmit={(e)=>createUser(e,all)}>
+                  <h2 style={{fontSize:"30px"}}>Sign Up</h2>
+            <article>
+            <input
+              type="name"
+              name="username"
+              placeholder="Username"
+              style={{
+                borderColor:all.usernameErr? 'red' : 'white',
+                padding: "10px",
+                borderRadius: "5px",
+              }}
+              onChange={onChangeFuncUsername}
+            />
+            <small >{all.usernameErr}</small>
+            </article>
+            <article>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              style={{
+                borderColor:all.emailErr? 'red' : 'white',
+                padding: "10px",
+                borderRadius: "5px",
+              }}
+              onChange={onChangeFuncEmail}
+            />
+            <small >{all.emailErr}</small>
+            </article>
+            <article>
+            <input
+              type="name"
+              name="fullName"
+              placeholder="Full name"
+              style={{
+                borderColor:all.fullnameErr? 'red' : 'white',
+                padding: "10px",
+                borderRadius: "5px",
+              }}
+              onChange={onChangeFuncFullname}
+            />
+            <small >{all.fullnameErr}</small>
+            </article>
+            <article>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              style={{
+                padding: "10px",
+                borderColor:all.passwordErr? 'red' : 'white',
+                borderRadius: "5px",
+              }}
+              onChange={onChangeFuncPassword}
+            />
+            <small >{all.passwordErr}</small>
+            </article>
+            <button
+              style={{
+                color:isDisabled ? '#707070' : 'white',
+                backgroundColor: isDisabled? '#eaeaea' : 'blue',
+                cursor:isDisabled? 'not-allowed' : 'pointer'
+              }}
+              disabled={isDisabled}
+              onClick={()=>handleLogin(all)}
+            >
+              {loading ? 'Loading ...' : 'Sign up'}
+            </button>
+            <p>Already Have An Account? <NavLink style={{color:'blue',cursor: "pointer", textDecoration: "none"}} to="/login">Login</NavLink></p>
+    
+          </form>
+        </main>
+          </div>
+        </div>
   )
 }
 
